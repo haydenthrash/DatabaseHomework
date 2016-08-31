56dc2bf38c291fa1744a9961ee2567ba326c94c6
Hayden Thrasher <haydenthrash@gmail.com>
2016-08-30 20:24:18 -0500

    General Clean up
    Added in methods from Dr.Gauch’s files on her site, the input.txt file,
    and did some general cleanup of the init commit.

--- databasehw1.cpp
+++ databasehw1.cpp
@@ -3,44 +3,134 @@
 #include <iostream>
 #include <string>
 #include <stdlib.h>
-
+#include <fstream>
 using namespace std;
- 
-int main()
-{ int choice, option; string file; void Menu(string file, int option);
-  cout<<"HELLO! Please enter the number of your choice\n(1) Open Database (2) Close Database\n";
-  cin >> choice;
 
-  if(choice == 1){
-     cout<<"Please enter the name of the database you wish to open\n";
-     cin >> file; //will have to make file an fstream not a string in order to open it
-     cout << "Please enter the number of your choice\n(1)View database\n(2)Delete entry\n(3)Add entry\n(4)Modify an entry\n(5)Create new Database\n(6)Quit\n";
-     cin >>option;
-     Menu(file, option);
-     }
+const int RECORD_SIZE = 71;
+const int NUM_RECORDS = 4110;
+const string FILENAME = "input.txt";
 
-  if(choice == 2){cout<<"Goodbye!"; exit(1); }
-  
+/*Get record number n-th (from 1 to NUM_RECORDS) */
+bool GetRecord(ifstream &Din, const int RecordNum,
+               string &Id, int &Experience, string &Married, float &Wage, string &Industry)
+{
+    bool Success = true;
+    string line;
 
-  //cout << "this is testing the exit code in option 2" << endl;
+    if ((RecordNum >= 1) && (RecordNum <= NUM_RECORDS))
+    {
+      Din.seekg(RecordNum * RECORD_SIZE, ios::beg);
+      Din >> Id >> Experience >> Married >> Wage >> Industry;
+    }
+    else
+    {
+      Success = false;
+    }
 
+    return Success;
+}
 
+/*Binary Search record id */
+bool binarySearch (ifstream &Din, const string Id,
+                   int &Experience, string &Married, float &Wage, string &Industry)
+{
+   int Low = 0;
+   int High = NUM_RECORDS;
+   int Middle;
+   string MiddleId;
 
+   bool Found = false;
+   while (!Found && (High >= Low))
+   {
+      Middle = (Low + High) / 2;
+      Din.seekg(Middle * RECORD_SIZE, ios::beg);
+      Din >> MiddleId >> Experience >> Married >> Wage >> Industry;
 
-}//ends main
+      if (MiddleId == Id)
+         Found = true;
+      else if (MiddleId < Id)
+         Low = Middle + 1;
+      else
+         High = Middle -1;
+   }
 
-void Menu(string filename, int option){
+   return Found;
+}
 
-  switch (option){
-	case 1: cout <<"will need to print out file to user in view mode only!!"; break;
-	case 2: cout <<"Delete an entry. This might be tricky but according to Dr.G we can fill it up with spaces instead of deleting everything and having to shift everyhting up or down"; break;
-	case 3: cout <<"Adding an entry should not be to hard. We would have to add it to a new file and after a certain count have the program merge and sort the file with the original database"; break;
-	case 4: cout <<"Modify an entry...I have no clue how to go about it"; break;
-	case 5: cout <<"Creating a new database shoud be easy. A few questions..we will want to have a count for the number of entries in the file, also sort the file as we go??"; break;
-	case 6: cout <<main(); break;
-	default: cout <<"You didn't enter a value option, please try again"; main(); break;
+void Menu(string filename, int menuOption){
+  switch (menuOption)
+  {
+  	case 1:
+      cout << "will need to print out file to user in view mode only!!";
+    break;
+  	case 2:
+      cout << "Delete an entry. This might be tricky but according to Dr.G we can fill it up with spaces instead of deleting everything and having to shift everyhting up or down";
+    break;
+  	case 3:
+      cout << "Adding an entry should not be to hard. We would have to add it to a new file and after a certain count have the program merge and sort the file with the original database";
+    break;
+  	case 4:
+      cout << "Modify an entry...I have no clue how to go about it";
+    break;
+  	case 5:
+      cout << "Creating a new database shoud be easy. A few questions..we will want to have a count for the number of entries in the file, also sort the file as we go??";
+    break;
+  	case 6:
+      cout << main();
+    break;
 
-
-   }//ends switch
-	
+  	default:
+      cout << "You didn't enter a valid option, please try again";
+      main();
+    break;
+  }//ends switch
 }//ends Menu function
+
+int main()
+{
+    int  openDatabase = 0, menuOption = 0;
+    string file;
+    bool shouldContinue = true;
+
+    void Menu(string file, int menuOption);
+
+    cout<<"HELLO! Please enter the number of your choice\n"
+        << "(1) Open Database (2) Close Database\n";
+    cin >> openDatabase;
+
+    if(openDatabase == 1)
+    {
+      cout << "Please enter the name of the database you wish to open\n";
+      cin >> file; //will have to make file an fstream not a string in order to open it
+
+      //while loop keeps menu up until they choose to quit
+       while(shouldContinue == true)
+       {
+         cout << "Please enter the number of your choice\n"
+              << "(1) View database\n"
+              << "(2) Delete entry\n"
+              << "(3) Add entry\n"
+              << "(4) Modify an entry\n"
+              << "(5) Create new Database\n"
+              << "(6) Quit\n";
+
+         cin >> menuOption;
+
+         if(menuOption == 6)
+         {
+           shouldContinue = false;
+         }
+
+         Menu(file, menuOption);
+       }
+    }
+
+    if(openDatabase == 2)
+    {
+      cout<<"Goodbye!";
+      exit(1);
+    }
+
+    //cout << "this is testing the exit code in option 2" << endl;
+    return 0;
+}//ends main
